1e4eceadde785b3d0a1cff4269fe1c1fbf38a122
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-07-25 17:04:05 -0600

    First commit

---
+++ src/CartApp.jsx
@@ -0,0 +1,6 @@
+
+export const CartApp = () => {
+  return (
+    <h1>CartApp!!!</h1>
+  );
+}
---
+++ src/FirstApp.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ( {title, subTitle, name} ) => {
+    return (
+        <>
+            {/* <code>{JSON.stringify(newMessage)}</code> */}
+            <h1>{ title }</h1>
+            <p>{ subTitle + 1}</p>
+            <p>{ name }</p>
+        </>
+    );
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string.isRequired
+}
+
+// Las defaultProps se ejecutan primero que las propTypes
+FirstApp.defaultProps = {
+    name: 'Luis Nieves!!!',
+    subTitle: 'Subtitulo opcional',
+    title: 'Titulo principal',
+}
