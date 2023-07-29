0f4e7f3e958c4027e63b405e13d45f172f563b6c
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-07-28 19:40:28 -0600

    test:(tests): uso de funciones getByText y getAllByText para comprobar existencia de elementos en un componente

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -4,7 +4,8 @@ export const FirstApp = ( {title, subTitle, name} ) => {
     return (
         <>
             {/* <code>{JSON.stringify(newMessage)}</code> */}
-            <h1> { title } </h1>
+            <h1 data-testid="test-title"> { title } </h1>
+            <p>{ subTitle }</p>
             <p>{ subTitle }</p>
             <p>{ name }</p>
         </>
--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -2,21 +2,42 @@ import { render } from '@testing-library/react';
 import { FirstApp } from '../src/FirstApp';
 
 describe('Pruebas en <FirstApp />', () => {
-    test('debe de hacer match con el snapshopt', () => {
+    // test('debe de hacer match con el snapshopt', () => {
 
-        const title = 'Hola, soy un test';
-        const { container } = render(<FirstApp title={ title} />);
-        expect( container ).toMatchSnapshot();
-    });
+    //     const title = 'Hola, soy un test';
+    //     const { container } = render(<FirstApp title={ title} />);
+    //     expect( container ).toMatchSnapshot();
+    // });
 
     test('debe de mostrar el titulo en un h1', () => {
 
         const title = 'Hola, soy un test';
-        const { container, getByText } = render(<FirstApp title={ title} />);
+        const { getByTestId, container, getByText } = render(<FirstApp title={ title} />);
         expect( getByText(title)).toBeTruthy();
 
         // Verificar que se encuentre dentro de un h1
-        const h1 = container.querySelector('h1');
-        expect( h1.innerHTML ).toContain(title);
+        // const h1 = container.querySelector('h1');
+        // expect( h1.innerHTML ).toContain(title);
+
+        // expect( getByTestId('test-title').innerHTML).toBe(title);
+        expect( getByTestId('test-title').innerHTML).toContain(title);
     });
+
+    test('debe de mostrar el subtitulo enviado por props', () => {
+
+        const title = 'Hola, soy un test';
+        const subTitle = 'Soy un subtitulo';
+        const { getByText, getAllByText } = render(
+            <FirstApp 
+                title={ title} 
+                subTitle={ subTitle }
+            />
+        );
+
+        // expect( getByText(subTitle) ).toBeTruthy();
+        expect( getAllByText(subTitle).length).toBe(2)
+
+    });
+
+
 })
